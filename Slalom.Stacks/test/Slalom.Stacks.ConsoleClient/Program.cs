e700f410b0eb4b3e65086a54b2962f3f72ee897c
George Olson <george.olson@windowslive.com>
2016-11-30 07:55:58 -0800

    no message

--- Slalom.Stacks/test/Slalom.Stacks.ConsoleClient/Program.cs
+++ Slalom.Stacks/test/Slalom.Stacks.ConsoleClient/Program.cs
@@ -8,145 +8,143 @@ using Slalom.Stacks.Communication.Logging;
 using Slalom.Stacks.Communication.Validation;
 using Slalom.Stacks.Configuration;
 using Slalom.Stacks.Domain;
-using Slalom.Stacks.EntityFramework;
 using Slalom.Stacks.Logging.Serilog;
-using Slalom.Stacks.Mongo;
 using Slalom.Stacks.Runtime;
 using Slalom.Stacks.Search;
 using Slalom.Stacks.Validation;
 
 namespace Slalom.Stacks.ConsoleClient
 {
-    public class SearchContext : DbContext
-    {
-        private readonly string _connectionString;
+    //public class SearchContext : DbContext
+    //{
+    //    private readonly string _connectionString;
 
-        public SearchContext()
-        {
-        }
+    //    public SearchContext()
+    //    {
+    //    }
 
-        public SearchContext(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
+    //    public SearchContext(string connectionString)
+    //    {
+    //        _connectionString = connectionString;
+    //    }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseSqlServer(_connectionString ?? "Data Source=localhost;Initial Catalog=Fit;Integrated Security=True");
-        }
+    //        optionsBuilder.UseSqlServer(_connectionString ?? "Data Source=localhost;Initial Catalog=Fit;Integrated Security=True");
+    //    }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<ItemSearchResult>()
-                        .ToTable("Items")
-                        .HasKey(e => e.Id);
+    //        modelBuilder.Entity<ItemSearchResult>()
+    //                    .ToTable("Items")
+    //                    .HasKey(e => e.Id);
 
-            modelBuilder.Entity<Audit>()
-                        .ToTable("Audits")
-                        .HasKey(e => e.Id);
+    //        modelBuilder.Entity<Audit>()
+    //                    .ToTable("Audits")
+    //                    .HasKey(e => e.Id);
 
-            modelBuilder.Entity<Log>()
-                        .ToTable("Logs")
-                        .HasKey(e => e.Id);
-        }
-    }
+    //        modelBuilder.Entity<Log>()
+    //                    .ToTable("Logs")
+    //                    .HasKey(e => e.Id);
+    //    }
+    //}
 
-    public class ItemSearchIndex : SearchIndex<ItemSearchResult>
-    {
-        public ItemSearchIndex(SearchContext context)
-            : base(context)
-        {
-        }
+    //public class ItemSearchIndex : SearchIndex<ItemSearchResult>
+    //{
+    //    public ItemSearchIndex(SearchContext context)
+    //        : base(context)
+    //    {
+    //    }
 
-        public IDomainFacade Domain { get; set; }
+    //    public IDomainFacade Domain { get; set; }
 
-        public override async Task RebuildIndexAsync()
-        {
-            await this.ClearAsync();
+    //    public override async Task RebuildIndexAsync()
+    //    {
+    //        await this.ClearAsync();
 
-            var index = 0;
-            var size = 1000;
+    //        var index = 0;
+    //        var size = 1000;
 
-            var set = this.Domain.CreateQuery<Item>();
+    //        var set = this.Domain.CreateQuery<Item>();
 
-            var working = set.Take(size).ToList();
-            while (working.Any())
-            {
-                await this.AddAsync(working.Select(e => new ItemSearchResult()).ToArray());
-                working = set.Skip(++index * size).Take(size).ToList();
-            }
-        }
-    }
+    //        var working = set.Take(size).ToList();
+    //        while (working.Any())
+    //        {
+    //            await this.AddAsync(working.Select(e => new ItemSearchResult()).ToArray());
+    //            working = set.Skip(++index * size).Take(size).ToList();
+    //        }
+    //    }
+    //}
 
-    public class Item : Entity, IAggregateRoot
-    {
-        public Item(string name)
-        {
-            this.Name = name;
-        }
+    //public class Item : Entity, IAggregateRoot
+    //{
+    //    public Item(string name)
+    //    {
+    //        this.Name = name;
+    //    }
 
-        public string Name { get; set; }
-    }
+    //    public string Name { get; set; }
+    //}
 
-    public class ItemRepository : MongoRepository<Item>
-    {
-        public ItemRepository(ItemMongoContext context) : base(context)
-        {
-        }
-    }
+    //public class ItemRepository : MongoRepository<Item>
+    //{
+    //    public ItemRepository(ItemMongoContext context) : base(context)
+    //    {
+    //    }
+    //}
 
-    public class ItemSearchResult : ISearchResult
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-    }
+    //public class ItemSearchResult : ISearchResult
+    //{
+    //    public Guid Id { get; set; } = Guid.NewGuid();
+    //}
 
-    public class CreateItemCommand : Communication.Command<ItemCreatedEvent>
-    {
-    }
+    //public class CreateItemCommand : Communication.Command<ItemCreatedEvent>
+    //{
+    //}
 
-    public class CreateItemCommandHandler : CommandHandler<CreateItemCommand, ItemCreatedEvent>
-    {
-        public override async Task<ItemCreatedEvent> Handle(CreateItemCommand command)
-        {
-            await this.Domain.AddAsync(new Item(command.CommandName));
+    //public class CreateItemCommandHandler : CommandHandler<CreateItemCommand, ItemCreatedEvent>
+    //{
+    //    public override async Task<ItemCreatedEvent> Handle(CreateItemCommand command)
+    //    {
+    //        await this.Domain.AddAsync(new Item(command.CommandName));
 
-            return new ItemCreatedEvent();
-        }
-    }
+    //        return new ItemCreatedEvent();
+    //    }
+    //}
 
-    public class ItemMongoContext : MongoDbContext
-    {
-        public ItemMongoContext()
-        {
-        }
-    }
+    //public class ItemMongoContext : MongoDbContext
+    //{
+    //    public ItemMongoContext()
+    //    {
+    //    }
+    //}
 
-    public class ItemSearchResultUpdater : IHandleEvent<ItemCreatedEvent>
-    {
-        private readonly ISearchFacade _facade;
+    //public class ItemSearchResultUpdater : IHandleEvent<ItemCreatedEvent>
+    //{
+    //    private readonly ISearchFacade _facade;
 
-        public ItemSearchResultUpdater(ISearchFacade facade)
-        {
-            _facade = facade;
-        }
+    //    public ItemSearchResultUpdater(ISearchFacade facade)
+    //    {
+    //        _facade = facade;
+    //    }
 
-        public Task Handle(ItemCreatedEvent instance, ExecutionContext context)
-        {
-            return _facade.AddAsync(new ItemSearchResult());
-        }
-    }
+    //    public Task Handle(ItemCreatedEvent instance, ExecutionContext context)
+    //    {
+    //        return _facade.AddAsync(new ItemSearchResult());
+    //    }
+    //}
 
-    public class must_do_this : BusinessValidationRule<CreateItemCommand>
-    {
-        protected override async Task<ValidationError> Validate(CreateItemCommand instance)
-        {
-            return "asdf";
-        }
-    }
+    //public class must_do_this : BusinessValidationRule<CreateItemCommand>
+    //{
+    //    protected override async Task<ValidationError> Validate(CreateItemCommand instance)
+    //    {
+    //        return "asdf";
+    //    }
+    //}
     
 
     public class Program
@@ -164,16 +162,6 @@ namespace Slalom.Stacks.ConsoleClient
             {
                 using (var container = new Container(typeof(Program)))
                 {
-                    //container.RegisterModule(new SerilogModule());
-                    container.RegisterModule(new LoggingModule<SearchContext>());
-                    container.Register(c => new SearchContext());
-
-                    new SearchContext().Database.Migrate();
-
-                    var result = container.Bus.Send(new CreateItemCommand()).Result;
-
-                    Console.WriteLine(result.IsSuccessful);
-                    Console.ReadKey();
                 }
             }
             catch (Exception exception)
