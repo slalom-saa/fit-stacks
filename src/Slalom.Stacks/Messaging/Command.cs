02bf46ea7a84a67bf6b6d06697934f8844276fe3
George Olson <george.olson@windowslive.com>
2017-01-21 10:25:35 -0800

    API updates.

--- src/Slalom.Stacks/Messaging/Command.cs
+++ src/Slalom.Stacks/Messaging/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Slalom.Stacks.Runtime;
+using System.Reflection;
+using System.Linq;
 using Slalom.Stacks.Utilities.NewId;
 
 namespace Slalom.Stacks.Messaging
@@ -12,8 +13,17 @@ namespace Slalom.Stacks.Messaging
     /// <seealso href="http://bit.ly/2d01rc7">Reactive Messaging Patterns with the Actor Model: Applications and Integration in Scala and Akka</seealso>
     public abstract class Command : ICommand
     {
-        private string _commandName;
-        private Type _type;
+        private readonly Lazy<RequestAttribute> _attribute;
+        private readonly Type _type;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command"/> class.
+        /// </summary>
+        protected Command()
+        {
+            _type = this.GetType();
+            _attribute = new Lazy<RequestAttribute>(() => _type.GetTypeInfo().GetCustomAttributes<RequestAttribute>().FirstOrDefault());
+        }
 
         /// <summary>
         /// Gets the identifier.
@@ -31,13 +41,13 @@ namespace Slalom.Stacks.Messaging
         /// Gets the command type.
         /// </summary>
         /// <value>The command type.</value>
-        Type ICommand.Type => _type ?? (_type = this.GetType());
+        Type ICommand.Type => _type;
 
         /// <summary>
         /// Gets the command name.
         /// </summary>
         /// <value>The command name.</value>
-        string ICommand.CommandName => _commandName ?? (_commandName = this.GetType().Name);
+        string ICommand.CommandName => this.GetCommandName();
 
         /// <summary>
         /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
@@ -81,6 +91,11 @@ namespace Slalom.Stacks.Messaging
         protected bool Equals(Command other)
         {
             return ((IMessage)this).Id.Equals(((IMessage)other).Id);
+        }
+
+        private string GetCommandName()
+        {
+            return _attribute.Value?.Name ?? _type.Name;
         }
     }
 }
