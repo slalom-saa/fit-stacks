faa2ec40779479d104fd830e031a95aa8bcfaa61
George Olson <georgeo@slalom.com>
2017-02-22 17:52:12 -0800

    ...

---
+++ test/Slalom.Stacks.ConsoleClient/Application/Products/Publish/PublishProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Slalom.Stacks.Messaging;
+
+namespace Slalom.Stacks.ConsoleClient.Application.Products.Publish
+{
+    public class PublishProductCommand : Command
+    {
+    }
+    
+    [Path("products/publish")]
+    public class PublishProduct : UseCase<PublishProductCommand>
+    {
+        public override void Execute(PublishProductCommand message)
+        {
+        }
+    }
+}
