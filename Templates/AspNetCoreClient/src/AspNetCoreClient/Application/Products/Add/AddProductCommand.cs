b3dbe9f42a579b84d75b1d233b97ddda449c9a83
George Olson <george.olson@windowslive.com>
2017-03-04 07:41:21 -0800

    ...

--- Templates/AspNetCoreClient/src/AspNetCoreClient/Application/Products/Add/AddProductCommand.cs
+++ Templates/AspNetCoreClient/src/AspNetCoreClient/Application/Products/Add/AddProductCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Slalom.Stacks.Messaging;
+using Slalom.Stacks.Services;
 
 namespace AspNetCoreClient.Application.Products.Add
 {
