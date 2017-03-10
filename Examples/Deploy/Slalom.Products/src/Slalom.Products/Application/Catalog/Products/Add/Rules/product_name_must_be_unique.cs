4517fedbf243fc87ff397a51bc72ea415d24c137
George Olson <george.olson@windowslive.com>
2017-03-10 06:27:49 -0800

    no message

---
+++ Examples/Deploy/Slalom.Products/src/Slalom.Products/Application/Catalog/Products/Add/Rules/product_name_must_be_unique.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Slalom.Products.Domain.Catalog.Products;
+//using Slalom.Stacks.Messaging.Validation;
+//using Slalom.Stacks.Validation;
+
+//namespace Slalom.Products.Application.Catalog.Products.Add.Rules
+//{
+//    public class product_name_must_be_unique : BusinessRule<AddProductCommand>
+//    {
+//        public override async Task<ValidationError> ValidateAsync(AddProductCommand instance)
+//        {
+//            if (await this.Domain.Exists<Product>(e => e.Name == instance.Name))
+//            {
+//                return "A product name must be unique.";
+//            }
+//            return null;
+//        }
+//    }
+//}
---
+++ Examples/Deploy/Slalom.Products/test/ConsoleClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Slalom.Products.Application.Catalog.Products.Add;
+using Slalom.Stacks;
+using Slalom.Stacks.Messaging;
+using Slalom.Stacks.Text;
+
+namespace ConsoleClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var stack = new Stack(typeof(AddProduct)))
+            {
+                stack.UseInMemoryPersistence();
+
+
+                var result = stack.Send(new AddProductCommand(null)).Result;
+
+                stack.Send("_systems/messaging/requests").Result.OutputToJson();
+
+                Console.WriteLine(result.ToJson());
+            }
+        }
+    }
+}
