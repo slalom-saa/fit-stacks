a0d9184f920218ccfa47836e76394e251a0ae6d2
George Olson <george.olson@windowslive.com>
2017-04-23 12:25:31 -0700

    Update document tools.

---
+++ assets/Remove-Packages.ps1
@@ -0,0 +1,19 @@
+﻿param (
+    $PackageId = "Slalom.Stacks.Data.MongoDb",
+    $ApiKey = "4fdba183-fa91-4650-8dae-760bf2c22337"
+)
+
+
+$lower = $PackageId.ToLowerInvariant();
+
+$json = Invoke-WebRequest -Uri "https://api.nuget.org/v3-flatcontainer/$lower/index.json" | ConvertFrom-Json
+
+foreach($version in $json.versions)
+{
+    if ($version -gt "1.6") {
+        Write-Host "latest" -ForegroundColor Yellow
+        #continue
+    }
+    Write-Host "Unlisting $PackageId, Ver $version"
+    Invoke-Expression "nuget delete $PackageId $version $ApiKey -source https://api.nuget.org/v3/index.json -NonInteractive"
+}
