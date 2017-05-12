0545cb36f0f38cb447025c7a0484d8dc834a6013
George Olson <georgeo@slalom.com>
2017-05-12 13:54:47 -0700

    Fix for issue #85

--- test/Slalom.Stacks.ConsoleClient/Program.cs
+++ test/Slalom.Stacks.ConsoleClient/Program.cs
@@ -15,6 +15,7 @@ using Slalom.Stacks.Security;
 using Slalom.Stacks.Services;
 using Slalom.Stacks.Services.Logging;
 using Slalom.Stacks.Services.Messaging;
+using Slalom.Stacks.Services.Validation;
 using Slalom.Stacks.Text;
 using Slalom.Stacks.Validation;
 
@@ -22,48 +23,15 @@ using Slalom.Stacks.Validation;
 
 namespace Slalom.Stacks.ConsoleClient
 {
-    public class Req
-    {
-        [Url("sdf")]
-        public string Url { get; set; } = "http://localhost:5001";
-
-        [Json("ad")]
-        public string Json { get; set; } = "{}";
-    }
-
-    public class Go : EndPoint<Req>
-    {
-        public override void Receive(Req instance)
-        {
-            base.Receive(instance);
-        }
-    }
-
-
     public class Program
     {
-
         [STAThread]
         public static void Main(string[] args)
         {
             try
             {
-                var content = "test";
-
-                var bytes = Encoding.UTF8.GetBytes(content);
-
-                Encoding.UTF8.GetString(Encryption.Decrypt(Encryption.Encrypt(bytes))).OutputToJson(); ;
-
-
-                return;
-
-                using (var stack = new Stack(typeof(AddProductCommand)))
+                using (var stack = new Stack())
                 {
-                    var config = stack.Container.Resolve<IConfiguration>();
-
-                    config.GetValue<string>("Authority").OutputToJson();
-
-                    //stack.Send(new Req()).Result.OutputToJson();
                 }
             }
             catch (Exception exception)
