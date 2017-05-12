3d43a616d407f3741921f0304208c8296e6dd546
George Olson <georgeo@slalom.com>
2017-05-11 21:36:30 -0700

    no message

--- src/Slalom.Stacks/Services/Inventory/RemoteEndPoint.cs
+++ src/Slalom.Stacks/Services/Inventory/RemoteEndPoint.cs
@@ -17,10 +17,12 @@ namespace Slalom.Stacks.Services.Inventory
         /// </summary>
         /// <param name="path">The relative path the endpoint.</param>
         /// <param name="fullPath">The full path the endpoint.</param>
-        public RemoteEndPoint(string path, string fullPath)
+        /// <param name="method">The endpoint method.</param>
+        public RemoteEndPoint(string path, string fullPath, string method = null)
         {
             this.Path = path;
             this.FullPath = fullPath;
+            this.Method = method;
         }
 
         /// <summary>
@@ -28,6 +30,12 @@ namespace Slalom.Stacks.Services.Inventory
         /// </summary>
         /// <value>The full path the endpoint.</value>
         public string FullPath { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the endpoint method.
+        /// </summary>
+        /// <value>The endpoint method.</value>
+        public string Method { get; set; }
 
         /// <summary>
         /// Gets the relative path the endpoint.  Used to identify the endpoint.
